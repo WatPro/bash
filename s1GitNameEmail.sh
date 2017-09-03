7932a6033d220cce0947922064aa9eb9076b7a76
Water Pro <@>
2017-09-03 15:15:23 +0000

    Create script to set up git user information

---
+++ s1GitNameEmail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script setup global user name and email 
+
+git config --global user.email "@"
+git config --global user.name "Water Pro"
+
+git config user.name
+git config user.email
