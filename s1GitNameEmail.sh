bd9138f0ead94093048cdcb1430849a20b63a38b
WatPro <31579668+WatPro@users.noreply.github.com>
2017-09-03 23:35:27 +0800

    Update s1GitNameEmail.sh

--- s1GitNameEmail.sh
+++ s1GitNameEmail.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script setup global user name and email 
+# This script sets up global user name and email 
 
 git config --global user.email "@"
 git config --global user.name "Water Pro"
