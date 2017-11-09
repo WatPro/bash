4a9fe3ef76b77c19a3f85995a021ce167d48242a
Water Pro <@>
2017-11-09 17:20:09 +0800

    Updated

--- CentOS/i_Git.sh
+++ CentOS/i_Git.sh
@@ -3,16 +3,19 @@
 ####################################################################################################
 ########## Install (up-to-date) Git                                                       ##########
 ########## Tested on CentOS 7                                                             ##########
+########## Dependencies: (None)                                                           ##########
 ####################################################################################################
+ 
+G_PATH=/usr/local
  
 yum -y install git
 yum -y groupinstall "Development Tools" 
 yum -y install openssl-devel libcurl-devel expat-devel perl-devel 
-cd /usr/ 
+cd $G_PATH
 git clone https://github.com/git/git
 cd git
 make 
-make prefix=/usr install
+make prefix=$G_PATH install
  
 git --version
 
