286ce9c69480669f5b878dcff31079a0c8dfd09b
Water Pro <@>
2017-11-10 11:55:22 +0800

    Changed yum to rpm

--- CentOS/i_Java.sh
+++ CentOS/i_Java.sh
@@ -6,12 +6,14 @@
 ########## Dependencies: (None)                                                           ##########
 ##########                                                                                ##########
 ####################################################################################################
+
+echo Downloading Java Development Kit
+####################################################################################################
 ########## Find out download link of Java for Linux:                                      ##########
 ########## http://www.oracle.com/technetwork/java/javase/downloads/index.html             ##########
 ####################################################################################################
-
-echo Downloading Java Development Kit
 JDK_URL=http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jdk-9.0.1_linux-x64_bin.rpm
+ 
 JDK_FILENAME=${JDK_URL##*/}
 JAVA_DIR=/usr/java/
 if [ ! -d $JAVA_DIR ]
@@ -20,9 +22,9 @@ then
 fi
 cd $JAVA_DIR
 curl --silent --location --header "Cookie: oraclelicense=accept-securebackup-cookie" --remote-name $JDK_URL
-yum --assumeyes localinstall $JDK_FILENAME
+rpm --upgrade -v --hash --replacepkgs $JDK_FILENAME
 rm --force $JDK_FILENAME 
 
-which java
+which java javac
 java --version
 
