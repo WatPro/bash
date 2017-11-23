d91427cc4fb4dbc9eb47ffe9507d1c133fe1fe97
Water Pro <@>
2017-11-23 13:57:00 +0800

    Updated Java installation script

--- CentOS/i_Java.sh
+++ CentOS/i_Java.sh
@@ -7,22 +7,22 @@
 ##########                                                                                ##########
 ####################################################################################################
 
-echo Downloading Java Development Kit
-####################################################################################################
-########## Find out download link of Java for Linux:                                      ##########
-########## http://www.oracle.com/technetwork/java/javase/downloads/index.html             ##########
-########## File Formate .rpm                                                              ##########
-####################################################################################################
-JDK_URL=http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/jdk-9.0.1_linux-x64_bin.rpm
+JAVA_PAGE0=http://www.oracle.com/technetwork/java/javase/downloads/index.html
+JAVA_HOST=`echo $JAVA_PAGE0 | sed --quiet 's/\(\(http[s]\?:\/\/\|\)[^\/]*\)\(\/.*\|\)/\1/p'`
+
+RE_EX='<h3 id=\"javasejdk\"><a name=\"\([^\"]*\)\" href=\"\([^\"]*\)\">\(.*\)<\/a><\/h3>'
+LINE=`curl --silent $JAVA_PAGE0 | grep "$RE_EX" | head --lines=1`
+VER=`echo $LINE | sed --quiet "s/.*$RE_EX.*/\1/p"`
+JAVA_PAGE1="$JAVA_HOST`echo $LINE | sed --quiet "s/.*$RE_EX.*/\2/p"`"
+JDK_URL=`curl --silent $JAVA_PAGE1 | sed --silent 's/.*"filepath":"\([^"]*.rpm\)".*/\1/p'` 
+
+echo "Downloading Java Development Kit ($VER) for Linux"
  
 JDK_FILENAME=${JDK_URL##*/}
 JAVA_DIR=/usr/java/
-if [ ! -d $JAVA_DIR ]
-then
-    mkdir $JAVA_DIR 
-fi
+mkdir --parents $JAVA_DIR 
 cd $JAVA_DIR
-curl --silent --location --header "Cookie: oraclelicense=accept-securebackup-cookie" --remote-name $JDK_URL
+curl --location --header "Cookie: oraclelicense=accept-securebackup-cookie" --remote-name $JDK_URL
 rpm --upgrade -v --hash --replacepkgs $JDK_FILENAME
 rm --force $JDK_FILENAME 
  
