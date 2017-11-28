4155d63135883556ff709b383fabeb4fc7760757
Water Pro <@>
2017-11-28 15:41:32 +0000

    Updated

--- CentOS/s_iptables_lastb.sh
+++ CentOS/s_iptables_lastb.sh
@@ -7,19 +7,28 @@
 ########## Tested on CentOS 7.3                                       ##########
 ################################################################################
 
+LOGDIR="$1"
 THRESHOLD=100
 LAST_IP=/tmp/last_ip_list
 LASTB_IP=/tmp/lastb_ip_list
 BLACKLIST=/tmp/blacklist_ip
 
-lastb -i | cut --characters=23-38 | tr --delete ' ' | grep --regexp='^[1-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' > "$LASTB_IP" 
- 
+writelog() {
+    if [ -w "$LOGDIR" ]
+    then
+        echo "$1" >> "$LOGDIR"
+    fi
+}
+
+writelog '##############################'
+lastb -i > "$LASTB_IP"
+writelog "`cat "$LASTB_IP" | tail --lines=1`"
+
+cat "$LASTB_IP" | cut --characters=23-38 | tr --delete ' ' | grep --regexp='^[1-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' > "$LASTB_IP" 
 last -i | cut --characters=23-38 | tr --delete ' ' | grep --regexp='^[1-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' > "$LAST_IP" 
- 
-iptables -L INPUT --numeric | awk 'NR<=2 || /^DROP /' | cut --characters=21-40 | tr --delete ' ' | grep --regexp='^[1-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' > "$BLACKLIST"
+iptables -L INPUT --numeric | awk 'NR<=2 || /^DROP /' | cut --characters=21-40 | tr --delete ' ' | grep --regexp='^[1-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}$' > "$BLACKLIST" 
 
-
-echo 'INPUT Frames Will Be DROPped From Below Sources: '
+echo 'INPUT Packets Will Be DROPped From Below Sources: '
 grep --invert-match --file="$LAST_IP" "$LASTB_IP" | grep --invert-match --file="$BLACKLIST" | sort --ignore-nonprinting --random-sort | uniq --count | sort --numeric-sort --reverse | while read line
 do
     COUNT=`echo "$line" | cut --fields=1 --delimiter=' '` 
@@ -30,7 +39,9 @@ do
     DEL_IP=`echo "$line" | cut --fields=2 --delimiter=' '` 
     iptables --append INPUT --source "$DEL_IP" --jump DROP 
     echo "$line"
+    writelog "$line"
 done
+writelog "`date`"
 
 iptables -L INPUT --numeric | awk 'NR<=2 || /^DROP /'
  
@@ -38,6 +49,6 @@ echo ' '
 echo 'The Failed Attempts Below Are Currently Whitelisted: ' 
 grep --file="$LAST_IP" "$LASTB_IP" | grep --invert-match --file="$BLACKLIST" | sort --ignore-nonprinting --random-sort | uniq --count | sort --numeric-sort --reverse
  
-#rm --force "$LAST_IP" "$LASTB_IP"
+#rm --force "$LAST_IP" "$LASTB_IP" "$BLACKLIST"
 
 
