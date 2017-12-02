dd63a202646f8411303b3b203d4aefd79c205059
root <@>
2017-12-02 14:49:28 +0000

    Add Conda installation

---
+++ CentOS/i_conda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ 
+CONDA_URL='https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh'
+CONDA_FILE=${CONDA_URL##*/}
+
+yum --assumeyes install bzip2
+
+curl --remote-name "$CONDA_URL" 
+bash "$CONDA_FILE" -b
+rm --force "$CONDA_FILE"
+ 
+#PATH="~/miniconda3/bin:$PATH"
+#conda install --yes jupyter
+ 
+
