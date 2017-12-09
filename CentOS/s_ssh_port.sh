7ccb76e9e27369f5b57ecfd0b8505f770d9d54e3
root <root@vultr.guest>
2017-12-09 12:17:28 +0000

    Added script to change SSH port

---
+++ CentOS/s_ssh_port.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+ 
+################################################################################
+##########     Change Port Used For SSH (Secure Shell) Service        ##########
+##########     Tested on CentOS 7                                     ##########
+################################################################################
+ 
+SSHD_CONFIG=/etc/ssh/sshd_config
+FIREWALLD_SSH=/usr/lib/firewalld/services/ssh.xml
+ 
+if [[ "$1" =~ ^[1-9][0-9]{0,4}$ && "$1" -le 65535 ]]
+then
+    PORT="$1"
+    sed --in-place "s/^[#]\?\(Port \)[1-9][0-9]\{0,4\}$/\1$PORT/" "$SSHD_CONFIG"
+    if [[ -e "$FIREWALLD_SSH" ]]
+    then 
+        sed --in-place "s/port=\"[1-9][0-9]\{0,4\}\"/port=\"$PORT\"/" "$FIREWALLD_SSH"
+        if [ running = "$(firewall-cmd --state 2>&1)" ] 
+        then
+            firewall-cmd --reload
+            firewall-cmd --list-services
+            firewall-cmd --info-service=ssh
+        fi 
+    fi
+    # restart SSH 
+    service sshd restart
+fi
+
+
