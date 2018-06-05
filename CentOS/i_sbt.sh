2c5aa477fdcce62377d01ae63b05182d4b18785d
WatPro <31579668+WatPro@users.noreply.github.com>
2018-06-05 17:54:47 +0800

    Create i_sbt.sh

---
+++ CentOS/i_sbt.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+if [ ! -n "`which javac`" ]
+then
+    sudo yum install openjdk-devel
+fi
+
+baseurl=`curl https://bintray.com/sbt/rpm/rpm | sed --silent 's/^baseurl=//p'`
+rpmfile=`curl --location "${baseurl}" | sed --silent 's/^.*"\(sbt[^"]*.rpm\)".*$/\1/p' | tail --lines=1`
+download_url="${baseurl}/${rpmfile}"
+curl --location ${download_url} --output ${rpmfile}
+
+
+
