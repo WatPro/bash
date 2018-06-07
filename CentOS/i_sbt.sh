21fb0e39d8f0bf8708f0a9506e36abd3ef91ab10
WatPro <31579668+WatPro@users.noreply.github.com>
2018-06-07 14:09:06 +0800

    Update i_sbt.sh

--- CentOS/i_sbt.sh
+++ CentOS/i_sbt.sh
@@ -8,6 +8,10 @@ fi
 baseurl=`curl https://bintray.com/sbt/rpm/rpm | sed --silent 's/^baseurl=//p'`
 rpmfile=`curl --location "${baseurl}" | sed --silent 's/^.*"\(sbt[^"]*.rpm\)".*$/\1/p' | tail --lines=1`
 download_url="${baseurl}/${rpmfile}"
+if [ ! -n "${download_url}" ]
+then
+    exit 1
+fi
 curl --location "${download_url}" --output "${rpmfile}"
 sudo yum --assumeyes install "${rpmfile}"
 rm --force "${rpmfile}"
