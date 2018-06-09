3ac808d568ea592da913b5c743b13667f9a6f8bf
WatPro <31579668+WatPro@users.noreply.github.com>
2018-06-09 17:52:22 +0800

    Scala (#1)
    * Create i_sbt.sh
    
    * Create i_Scala.sh
    
    * Update i_Scala.sh
    
    * Update i_Scala.sh
    
    * Update i_sbt.sh
    
    * Update i_sbt.sh
    
    * Update i_Scala.sh
    
    * Update i_sbt.sh
    
    * Update i_Scala.sh
    
    * Update i_sbt.sh
    
    * Update i_sbt.sh

---
+++ CentOS/i_Scala.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+if [ ! -n "`which javac`" ]
+then
+    sudo yum --assumeyes install java-devel
+fi
+
+scala_web='https://scala-lang.org/download/'
+scala_rpm=`curl "${scala_web}" | sed --silent 's/^.*"\(http[s]:\/\/.*\.rpm\)".*$/\1/p'`
+if [ ! -n "${scala_rpm##*/}" ]
+then
+    exit 1
+fi
+download_url="${baseurl}/${rpmfile}"
+curl --location "${scala_rpm}" --output "${scala_rpm##*/}"
+sudo yum --assumeyes install "${scala_rpm##*/}"
+rm --force "${scala_rpm##*/}"
+
