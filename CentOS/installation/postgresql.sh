08273195af5fc86992c3c9aa0d040e13457c691e
WatPro <31579668+WatPro@users.noreply.github.com>
2018-07-16 15:36:17 +0800

    Update postgresql.sh

--- CentOS/installation/postgresql.sh
+++ CentOS/installation/postgresql.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+
+## https://www.postgresql.org/docs/current/static/install-short.html
 yum --assumeyes install postgresql-server
 
 USER_NAME='postgres'
@@ -7,6 +9,16 @@ PGDATA='/usr/local/pgsql/data/'
 adduser "${USER_NAME}"
 mkdir --parent "${PGDATA}"
 chown  "${USER_NAME}" "${PGDATA}"
+## 
 su - "${USER_NAME}"
 initdb --pgdata="${PGDATA}"
+
+################################################################################
+########  start the database server using:                              ########
+########      postgres -D /var/lib/pgsql/data                           ########
+########  or                                                            ########
+########      pg_ctl -D /var/lib/pgsql/data -l logfile start            ########
+################################################################################
+
+pg_ctl --pgdata="${PGDATA}" --log='/var/log/postgres_logfile.log' start
 
