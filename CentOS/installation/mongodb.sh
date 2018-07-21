0d4fb6bad4d49c3f2fa18f70e8e90c1fb93b5823
WatPro <31579668+WatPro@users.noreply.github.com>
2018-07-21 14:40:53 +0800

    Update mongodb.sh

--- CentOS/installation/mongodb.sh
+++ CentOS/installation/mongodb.sh
@@ -16,7 +16,7 @@ cd /usr/local
 ########## Figure out the variables used in Yellowdog Updater, Modified:                  ##########
 ########## # python2 -c 'import yum; yb=yum.YumBase(); print(yb.conf.yumvar)'             ##########
 ####################################################################################################
-F_PATH=https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.4/x86_64/RPMS/
+F_PATH='https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.4/x86_64/RPMS/'
 FILES=(mongodb-org-mongos-3.4.10-1.el7.x86_64.rpm mongodb-org-server-3.4.10-1.el7.x86_64.rpm mongodb-org-shell-3.4.10-1.el7.x86_64.rpm mongodb-org-tools-3.4.10-1.el7.x86_64.rpm mongodb-org-3.4.10-1.el7.x86_64.rpm)
 
 for FILE in ${FILES[@]}
