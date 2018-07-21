ed650c1d1a99aafeca82fc546f155574ef6ac5cc
WatPro <31579668+WatPro@users.noreply.github.com>
2018-07-21 15:45:04 +0800

    Update mongodb.sh

--- CentOS/installation/mongodb.sh
+++ CentOS/installation/mongodb.sh
@@ -16,6 +16,10 @@ cd /usr/local
 ########## Figure out the variables used in Yellowdog Updater, Modified:                  ##########
 ########## # python2 -c 'import yum; yb=yum.YumBase(); print(yb.conf.yumvar)'             ##########
 ####################################################################################################
+REDHAT_URL='https://repo.mongodb.org/yum/redhat/7/mongodb-org/'
+MDB_VERSION=`curl "${REDHAT_URL}" | sed --silent "s/^.*href='\([0-9]\+\(\.[0-9]\+\)\?\)'.*$/\1/p" | sort --numeric-sort | tail --lines=1 `
+MDB_DIR_URL="${REDHAT_URL}${MDB_VERSION}/x86_64/RPMS/"
+
 F_PATH='https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.4/x86_64/RPMS/'
 FILES=(mongodb-org-mongos-3.4.10-1.el7.x86_64.rpm mongodb-org-server-3.4.10-1.el7.x86_64.rpm mongodb-org-shell-3.4.10-1.el7.x86_64.rpm mongodb-org-tools-3.4.10-1.el7.x86_64.rpm mongodb-org-3.4.10-1.el7.x86_64.rpm)
 
