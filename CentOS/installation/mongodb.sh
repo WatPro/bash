516be7be3d9b2417838a370894f05c2bde5f2f46
WatPro <31579668+WatPro@users.noreply.github.com>
2018-12-02 15:47:36 +0800

    Rearrange (#4)
    * Create README.md
    
    * Rename CentOS/i_Java.sh to CentOS/installation/Java.sh
    
    * Rename CentOS/i_Scala.sh to CentOS/installation/Scala.sh
    
    * Rename CentOS/i_sbt.sh to CentOS/installation/sbt.sh
    
    * Update and rename CentOS/i_conda.sh to CentOS/installation/conda.sh
    
    * Rename CentOS/i_Git.sh to CentOS/installation/Git.sh
    
    * Update README.md
    
    * Delete i_Nmap.sh
    
    * Delete i1MavenJava.sh
    
    * Rename CentOS/i_Python3.sh to CentOS/installation/Python3.sh
    
    * Delete i2init.sh
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update sbt.sh
    
    * Update sbt.sh
    
    * Update Python3.sh
    
    * Rename Python3.sh to python3.sh
    
    * Rename Git.sh to git.sh
    
    * Rename Scala.sh to scala.sh
    
    * Create postgresql.sh
    
    * Rename Java.sh to java.sh
    
    * Update postgresql.sh
    
    * Update postgresql.sh
    
    * Update postgresql.sh
    
    * Update postgresql.sh
    
    * Update postgresql.sh
    
    * Rename CentOS/i_MongoDB.sh to CentOS/installation/mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh
    
    * Update mongodb.sh

--- CentOS/installation/mongodb.sh
+++ CentOS/installation/mongodb.sh
@@ -2,7 +2,7 @@
 
 ####################################################################################################
 ########## Install MongoDB (manually)                                                     ##########
-########## Tested on CentOS 7.4                                                           ##########
+########## Tested on CentOS 7.4 (new changes are not tested yet)                          ##########
 ########## Dependencies: (None)                                                           ##########
 ####################################################################################################
  
@@ -16,19 +16,30 @@ cd /usr/local
 ########## Figure out the variables used in Yellowdog Updater, Modified:                  ##########
 ########## # python2 -c 'import yum; yb=yum.YumBase(); print(yb.conf.yumvar)'             ##########
 ####################################################################################################
-F_PATH=https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.4/x86_64/RPMS/
-FILES=(mongodb-org-mongos-3.4.10-1.el7.x86_64.rpm mongodb-org-server-3.4.10-1.el7.x86_64.rpm mongodb-org-shell-3.4.10-1.el7.x86_64.rpm mongodb-org-tools-3.4.10-1.el7.x86_64.rpm mongodb-org-3.4.10-1.el7.x86_64.rpm)
-
+releasever=`python2 -c 'import yum; yb=yum.YumBase(); print(yb.conf.yumvar)' | sed --silent "s/^.*'releasever':[ ]*'\([^']\+\)'.*$/\1/p"`
+REDHAT_URL="https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/"
+MDB_VERSION=`curl "${REDHAT_URL}" | sed --silent "s/^.*href='\([0-9]\+\(\.[0-9]\+\)\?\)'.*$/\1/p" | sort --version-sort | tail --lines=1 `
+MDB_DIR_URL="${REDHAT_URL}${MDB_VERSION}/x86_64/RPMS/"
+MDB_RPM_LIST=`curl "${MDB_DIR_URL}" | sed --silent "s/^.*href='\([^']\+\.rpm\)'.*$/\1/p"`
+FILES=(mongodb-org-mongos mongodb-org-server mongodb-org-shell mongodb-org-tools mongodb-org)
+FILENAMES=()
 for FILE in ${FILES[@]}
 do
-    echo Downloading $FILE
-    curl --remote-name $F_PATH$FILE
+    FILENMAE=`echo "${MDB_RPM_LIST}" | grep "${FILE}-${MDB_VERSION}" | sort --version-sort | tail --lines=1 `
+    FILENAMES+=("${FILENMAE}")
+    echo Downloading "${MDB_DIR_URL}${FILENMAE}"
+    curl "${MDB_DIR_URL}${FILENMAE}" --output "${FILENMAE}"
 done
 
-rpm --upgrade -v --hash --replacepkgs ${FILES[@]} 
+rpm --upgrade -v --hash --replacepkgs ${FILENAMES[@]} 
 
-for FILE in ${FILES[@]}
+for FILENAME in ${FILENAMES[@]}
 do
-    rm --force ./$FILE
+    rm --force "./$FILENAME"
 done
+ 
+mongo --version
+ 
+sudo systemctl start mongod
 
+ 
