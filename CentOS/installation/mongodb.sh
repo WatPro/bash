bd663bf6b6fa8dd131ab6cbefd5ab69782363d36
WatPro <31579668+WatPro@users.noreply.github.com>
2018-12-29 22:52:29 +0800

    Update mongodb.sh

--- CentOS/installation/mongodb.sh
+++ CentOS/installation/mongodb.sh
@@ -12,13 +12,14 @@ cd /usr/local
 ########## Set up path and filenames                                                      ##########
 ########## Read the articles:                                                             ##########
 ########## https://docs.mongodb.com/manual/tutorial/install-mongodb-on-red-hat/           ##########
+########## ## Odd-numbered minor release versions (e.g. 4.1) are unsuitable for production use #####
 ########## https://www.digitalocean.com/community/tutorials/how-to-install-mongodb-on-centos-7 #####
 ########## Figure out the variables used in Yellowdog Updater, Modified:                  ##########
 ########## # python2 -c 'import yum; yb=yum.YumBase(); print(yb.conf.yumvar)'             ##########
 ####################################################################################################
 releasever=`python2 -c 'import yum; yb=yum.YumBase(); print(yb.conf.yumvar)' | sed --silent "s/^.*'releasever':[ ]*'\([^']\+\)'.*$/\1/p"`
 REDHAT_URL="https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/"
-MDB_VERSION=`curl "${REDHAT_URL}" | sed --silent "s/^.*href='\([0-9]\+\(\.[0-9]\+\)\?\)'.*$/\1/p" | sort --version-sort | tail --lines=1 `
+MDB_VERSION=`curl "${REDHAT_URL}" | sed --silent "s/^.*href='\([0-9]\+\.[0-9]*[02468]\)'.*$/\1/p" | sort --version-sort | tail --lines=1 `
 MDB_DIR_URL="${REDHAT_URL}${MDB_VERSION}/x86_64/RPMS/"
 MDB_RPM_LIST=`curl "${MDB_DIR_URL}" | sed --silent "s/^.*href='\([^']\+\.rpm\)'.*$/\1/p"`
 FILES=(mongodb-org-mongos mongodb-org-server mongodb-org-shell mongodb-org-tools mongodb-org)
